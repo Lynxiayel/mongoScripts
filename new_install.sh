65a80d4251233300d850b1d9c2e11f144bccdbe1
lynxiayel <lxytcmn@gmail.com>
2014-04-02 14:33:52 -0400

    add command line support for install path

--- new_install.sh
+++ new_install.sh
@@ -8,17 +8,33 @@ fileName="mongodb-linux-$arch-${version}"
 downloadFile="${fileName}.tgz"
 #download url
 dlurl="http://fastdl.mongodb.org/linux/$downloadFile"
+
 #install path
-installPath=$HOME
+if [ $# -gt 0 ];then
+    installPath=$1
+else
+    read -p "Please input the installation path (enter to use home directory):" inputPath
+    if [ -z $inputPath ];then
+        installPath=$HOME
+    else
+        installPath=$inputPath
+    fi
+fi
+
+#download install file
 if [ ! -f $downloadFile ]; then
     echo "download installation file..."
     curl -O $dlurl
     echo "Download done. "
 fi
+
+#extract
 echo "Extracting..."
 tar zxf $downloadFile
 echo "Extracting done."
 extracted=`ls |grep -m 1 "$fileName"`
+
+#move to target path
 if [ -d $extracted ];then
     echo $extraced
     mv $extracted $installPath
